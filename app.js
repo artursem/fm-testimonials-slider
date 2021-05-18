5e8cd856b6f80f2184ee941e882c062969811a8b
artursem <artur.semeniuk@gmail.com>
2021-05-18 21:34:55 +0200

    add keyboard navigation

--- app.js
+++ app.js
@@ -2,10 +2,32 @@ const slide = document.querySelector('.slide');
 const btnPrev = document.querySelector('#btn-prev');
 const btnNext = document.querySelector('#btn-next');
 
-btnPrev.addEventListener('click', () => {
+const prev = () => {
     slide.style.transform='translateX(-600px)';
-});
+}
 
-btnNext.addEventListener('click', () => {
+const next = () => {
     slide.style.transform='translateX(600px)';
+}
+
+const btnOp = (btn) => {
+    btn.classList.remove('btn-solid');
+    btn.classList.add('btn-op');
+        setTimeout(()=>{
+            btn.classList.remove('btn-op');
+            btn.classList.add('btn-solid');
+        },300);
+}
+
+btnPrev.addEventListener('click', prev);
+btnNext.addEventListener('click', next);
+document.addEventListener('keyup', e => {
+    if (e.key === "ArrowLeft") {
+        prev();
+        btnOp(btnPrev);
+    };
+    if (e.key === "ArrowRight") {
+        next();
+        btnOp(btnNext);
+    };
 });
