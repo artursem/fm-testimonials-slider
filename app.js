92116505436f634a8a56cc0eef2d5df2f555dec4
artursem <artur.semeniuk@gmail.com>
2021-05-18 21:11:35 +0200

    add card animation

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hello!');
+const slide = document.querySelector('.slide');
+const btnPrev = document.querySelector('#btn-prev');
+const btnNext = document.querySelector('#btn-next');
+
+btnPrev.addEventListener('click', () => {
+    slide.style.transform='translateX(-600px)';
+});
+
+btnNext.addEventListener('click', () => {
+    slide.style.transform='translateX(600px)';
+});
